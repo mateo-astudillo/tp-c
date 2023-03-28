3c34a81b6e962b8e7e3dc7e20822b65a1a6c2c08
Mateo Astudillo <ast.mateo@gmail.com>
2023-03-28 10:28:58 -0300

    add exercise 15, charge routes

--- main.c
+++ main.c
@@ -9,29 +9,45 @@
 #define CLEAR "clear"
 #endif
 
+
+void exercise15() {
+	printf(" Ejercicio 15\n");
+	int routes[5][5];
+	for (int i=0; i<5; i++) {
+		for (int j=i; j<5; j++) {
+			if (i == j)
+				continue;
+			printf(" Distancia entre ruta %d y %d\n > ", i+1, j+1);
+			scanf("%d", &routes[i][j]);
+			routes[j][i] = routes[i][j];
+		}
+	}
+}
+
+
 int main() {
 	int menu = 1;
 	char *options[] = {
 		"0 - Salir",
-		"1 - Ejercicio 12",
+		"1 - Ejercicio 15",
 		"2 - Ejercicio 13",
 		"3 - Ejercicio 14"
 	};
 	int options_length = sizeof(options) / sizeof(options[0]);
 	while(menu) {
 		for (int i=0; i<options_length; i++)
-			printf("%s\n", options[i]);
+			printf(" %s\n", options[i]);
 		printf(" > ");
 		scanf("%d", &menu);
 		switch(menu) {
 			case 1:
-				printf("Ejercicio 12\n");
+				exercise15();
 				break;
 			case 2:
-				printf("Ejercicio 13\n");
+				printf(" Ejercicio 13\n");
 				break;
 			case 3:
-				printf("Ejercicio 14\n");
+				printf(" Ejercicio 14\n");
 				break;
 		}
 	}
