aba96eb5b78ed1ce8b53f526cce4cfb3edae9c2a
Tiven <vairtor@gmail.com>
2023-03-30 17:40:11 -0300

    ♻️  Refactor exercise 13 with functions

--- src/exercise-13.c
+++ src/exercise-13.c
@@ -1,11 +1,6 @@
 #include "../inc/exercises.h"
 
-void exercise13() {
-	printf(" Ejercicio 13\n");
-	int students = 5, grades = 2;
-	float mathNotes[grades][students], average = 0, total = 0, max = 0;
-
-
+static void setNotes(int grades, int students, float **mathNotes){
 	for (int i = 0; i < grades; i++) {
 		printf(" Grado %d: \n", i+1);
 		for (int j = 0; j < students; j++){
@@ -13,8 +8,10 @@ void exercise13() {
 			mathNotes[i][j] = getFloat(1, 10);
 		}	
 	}
+}
 
-
+static void getMaxNote(int grades, int students, float **mathNotes){
+	float  max = 0;
 	for (int i = 0; i < grades; i++) {
 		for (int j = 0; j < students; j++) {
 			if (max <= mathNotes[i][j])
@@ -22,36 +19,81 @@ void exercise13() {
 		}
 	}
 	printf(" La nota mayor es: %.2f\n", max);
-	
+}
 
+static void getAverage(int grades, int students, float **mathNotes){
+	float average, total;
 	printf(" Promedio de notas: \n");
 	for (int i = 0; i < grades; i++) {
-		for (int j = 0; j < students; j++)
-		{
+		for (int j = 0; j < students; j++) {
 			total += mathNotes[i][j];
 		}
 		average = total / students;
 		total = 0;
 		printf(" El promedio de grado %d es: %.2f\n", i, average);
 	}
-	
+}
+
+static void showNotes(int grades, int students, float **mathNotes){
 	printf(" Notas de alumnos\n");
 	for (int i = 0; i < grades; i++) {
 		printf(" Grado %d: \n", i+1);
 		for (int j = 0; j < students; j++)
 			printf(" Alumno %d : %.2f \n", j+1, mathNotes[i][j]);
 	}
-	system("pause");
 }
 
-/*
-	Se pide cargar en una matriz las notas de matemáticas de los alumnos de
-	secundaria de una escuela. Suponiendo que hay 2 grados distintos, 5 alumnos
-	por grado, se pide:
-	i. Ingresar las notas que ha sacado cada alumno de cada grado. 🟢
-	ii. Imprimir cuál es la nota promedio de cada grado. 🟢
-	iii. Imprimir la mayor nota en general. 🟢
-	iv. Imprimir todas las notas. 🟢
-	v. Imprimir cuántos alumnos aprobaron (sacaron mayor a 7) y
-	cuántos reprobaron de cada grado
-*/
+static void showApprovedStudents(){}
+
+void exercise13() {
+	printf(" Ejercicio 13\n");
+	int students = 5, grades = 2, option = 1, stateNotes = 0;
+	float **mathNotes = (float **) malloc(students * grades * sizeof(float *));
+
+	char *options[] = {
+		"0 - Salir",
+		"1 - Cargar Notas",
+		"2 - Mostrar nota promedio",
+		"3 - Mostrar la mayor nota general",
+		"4 - Mostrar todas las notas",
+		"5 - Alumnos aprobados"
+	};
+
+	int options_length = sizeof(options) / sizeof(options[0]);
+
+	while (option != 0) {
+		if (!stateNotes) {
+			setNotes(grades, students, mathNotes);
+			stateNotes = 1;
+		}
+		option = menu(0, options_length, options);
+		switch (option) {
+			case 0:
+				printf(" Fin\n");
+				break;
+			case 1:
+				setNotes(grades, students, mathNotes);
+				break;
+			case 2:
+				getAverage(grades, students, mathNotes);
+				break;
+			case 3:
+				getMaxNote(grades, students, mathNotes);
+				break;
+			case 4:
+				showNotes(grades, students, mathNotes);
+				break;
+			case 5:
+				showApprovedStudents();
+				break;
+			default:
+				break;
+		}
+	}
+
+	for (int i = 0; i < grades*students; i++) {
+        free(mathNotes[i]);
+    }
+    free(mathNotes);
+	
+}
