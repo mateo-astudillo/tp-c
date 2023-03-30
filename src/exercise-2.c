80e4adb404b3b763582c437564f0ab11ef707c2b
Tiven <vairtor@gmail.com>
2023-03-30 20:07:46 -0300

    ✨ Exercice 2 show a triangle

--- src/exercise-2.c
+++ src/exercise-2.c
@@ -2,4 +2,19 @@
 
 void exercise2() {
 	printf(" Ejercicio 2\n");
+	int lines;
+	char letter[2];
+	
+	printf("Ingresa una letra: ");
+	scanf("%s",letter);
+
+    printf("Ingresa la cantidad de lineas para el triangulo: ");
+    scanf("%d", &lines);
+
+      for (int i = lines; i >= 1; i--) {
+        for (int j = 0; j < i; j++) {
+            printf("%s", letter);
+        }
+        printf("\n");
+    }
 }
