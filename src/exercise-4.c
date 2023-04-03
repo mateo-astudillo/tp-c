6a7e0155afa203eff80345d4e2eff5c8a2508dc6
Mateo Astudillo <ast.mateo@gmail.com>
2023-04-03 11:52:49 -0300

    🎨 Format code exercise 4

--- src/exercise-4.c
+++ src/exercise-4.c
@@ -1,61 +1,50 @@
 #include "../inc/exercises.h"
 
-static int isInteger(char *integer, int length)
-{
+static int isInteger(char *integer, int length) {
 	int i = 0;
-	while(integer[i] != '\0' && i < length) // As long as the loop doesn't detect the end of the string, it'll continuing iterating
-	{
-		if (integer[0] == '-' && integer[i+1] != '\0') // If at the first position detect a slash "-" and its next position ain't the end of the string, it'll check if only there's a valid negative number
-		{
-			if (isdigit(integer[i+1]))
-			{
-				if (i == length) // Once the loop reaches the end of the string and the number is a valid negative number, it'll return 0
-				{
+	// As long as the loop doesn't detect the end of the string, it'll continuing iterating
+	while(integer[i] != '\0' && i < length) {
+		// If at the first position detect a slash "-" and its next position ain't the end of the string, it'll check if only there's a valid negative number
+		if (integer[0] == '-' && integer[i+1] != '\0') {
+			if (isdigit(integer[i+1])) {
+				// Once the loop reaches the end of the string and the number is a valid negative number, it'll return 0
+				if (i == length) {
 					return 0;
 					break;
 				}
-			}
-			else // Otherwise, return 1 that means invalid number
-			{
+			// Otherwise, return 1 that means invalid number
+			} else {
 				return 1;
 				break;
 			}
-		}
-		else if (!isdigit(integer[i])) // If detect that isn't a valid number, it'll break the loop returning 1
-        {
-            return 1;
-            break;
-        }
-		i++;
+		// If detect that isn't a valid number, it'll break the loop returning 1
+		} 
+		if (!isdigit(integer[i])) {
+	    return 1;
+	    break;
     }
-    return 2;
+	i++;
+  }
+  return 2;
 }
 
 void exercise4() {
 	char temp[100];
 	int number = 0;
-
 	system(CLEAR);
 	printf(" Ejercicio 4\n");
 	printf("Digite un numero: ");
 	scanf("%s", temp);
-
-	if (isInteger(temp, sizeof(temp)) == 0 || isInteger(temp, sizeof(temp)) == 2)
-	{
+	if (isInteger(temp, sizeof(temp)) == 0 || isInteger(temp, sizeof(temp)) == 2) {
 		number = atoi(temp);
-
-		if (number != 0)
-		{
+		if (number != 0) {
 			exercise4();
-		}
-		else if (number == 0)
-		{
+		} 
+		if (number == 0) {
 			printf("Se ha detectado un numero '0', hasta luego.");
 			slp(2);
 		}
-	}
-	else
-	{
+	} else {
 		printf("El numero introducido no es valido, solo se admiten numeros enteros positivos o negativos.");
 		slp(2);
 	}
